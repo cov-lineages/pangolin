031fd1485230ef02cd4237eaeaa0f0cd4d8bcead
aineniamh <aine.otoole@ed.ac.uk>
2020-09-14 17:10:13 +0100

    new header and model names

--- pangolin/command.py
+++ pangolin/command.py
@@ -232,9 +232,9 @@ you must have files ending in putative.fasta.treefile\nExiting.""")
 
         for r,d,f in os.walk(data_dir):
             for fn in f:
-                if fn == "pangolearnDecisionTreeHeaders_v1.joblib":
+                if fn == "decisionTreeHeaders_v1.joblib":
                     header_file = os.path.join(r, fn)
-                elif fn == "pangolearnDecisionTree_v1.joblib":
+                elif fn == "decisionTree_v1.joblib":
                     trained_model = os.path.join(r, fn)
                 elif fn == "lineages.metadata.csv":
                     lineages_csv = os.path.join(r, fn)
