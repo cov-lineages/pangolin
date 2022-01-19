a985370455c6882873d4c23b95a1b99a5b7e919c
aineniamh <aine.otoole@ed.ac.uk>
2022-01-19 16:29:22 +0000

    updting out error

--- pangolin/utils/error_messages.py
+++ pangolin/utils/error_messages.py
@@ -2,7 +2,7 @@ import sys
 from pangolin.utils.log_colours import green,cyan
 
 
-def install_error(package, url)
+def install_error(package, url):
     sys.stderr.write(cyan(f'Error: please install `{package}` with \n') +
     f"pip install git+{url}")
     sys.exit(-1)
