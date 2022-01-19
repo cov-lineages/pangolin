c10046e7866f7b7a62d07d77c5f864c07662f1d2
aineniamh <aine.otoole@ed.ac.uk>
2022-01-19 15:02:31 +0000

    adding location for error messages for tidier code base

---
+++ pangolin/utils/error_messages.py
@@ -0,0 +1,8 @@
+import sys
+from pangolin.utils.log_colours import green,cyan
+
+
+def install_error(package, url)
+    sys.stderr.write(cyan(f'Error: please install `{package}` with \n') +
+    f"pip install git+{url}")
+    sys.exit(-1)
