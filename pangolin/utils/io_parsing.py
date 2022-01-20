b1c6a83333bd33961781344add3c8b4f5ef642c2
aineniamh <aine.otoole@ed.ac.uk>
2022-01-20 09:56:02 +0000

    io parsing

--- pangolin/utils/io_parsing.py
+++ pangolin/utils/io_parsing.py
@@ -4,6 +4,10 @@ from pangolin.utils.log_colours import green,cyan
 import select
 
 
+from tempfile import TemporaryDirectory, TemporaryFile, gettempdir, tempdir
+import tempfile
+
+
 def find_query_file(cwd, query_arg):
     if len(query_arg) > 1:
         print(cyan(f"Error: Too many query (input) fasta files supplied: {query_arg}\nPlease supply one only."))
@@ -36,3 +40,56 @@ def find_query_file(cwd, query_arg):
             'Please enter your fasta sequence file and refer to pangolin usage at: https://cov-lineages.org/pangolin.html' +
             ' for detailed instructions.\n'))
         sys.exit(-1)
+
+def set_up_outdir(outdir_arg,cwd,outdir):
+    if outdir_arg:
+        outdir = os.path.join(cwd, outdir_arg)
+        if not os.path.exists(outdir):
+            try:
+                os.mkdir(outdir)
+            except:
+                sys.stderr.write(cyan(f'Error: cannot create directory:') + f"{outdir}")
+                sys.exit(-1)
+    return outdir
+
+def set_up_outfile(outfile_arg, outfile, outdir):
+    if outfile_arg:
+        outfile = os.path.join(outdir, outfile_arg)
+    else:
+        outfile = os.path.join(outdir, outfile)
+    return outfile
+
+
+def set_up_tempdir(tempdir_arg,no_temp_arg,cwd,outdir):
+
+    if tempdir_arg:
+        to_be_dir = os.path.join(cwd, tempdir_arg)
+        if not os.path.exists(to_be_dir):
+            os.mkdir(to_be_dir)
+        temporary_directory = tempfile.TemporaryDirectory(suffix=None, prefix=None, dir=to_be_dir)
+        tempdir = temporary_directory.name
+    else:
+        temporary_directory = tempfile.TemporaryDirectory(suffix=None, prefix=None, dir=None)
+        tempdir = temporary_directory.name
+
+    if no_temp_arg:
+        print(green(f"\n--no-temp: ") + f"all intermediate files will be written to {outdir}\n")
+        tempdir = outdir
+
+    return tempdir
+
+
+def parse_alignment_options(alignment_arg, outdir, tempdir,alignment_file_arg, alignment_file):
+    if alignment_arg:
+        aligndir = outdir
+        alignment_out = True
+    else:
+        aligndir = tempdir
+        alignment_out = False
+
+    if alignment_file_arg:
+        alignment_file = os.path.join(aligndir, alignment_file_arg)
+    else:
+        alignment_file = os.path.join(aligndir, alignment_file)
+
+    return alignment_file,alignment_out
