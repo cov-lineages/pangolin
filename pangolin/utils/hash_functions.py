7882123fef59d9fa0f01b728ff4fe91fefe92992
aineniamh <aine.otoole@ed.ac.uk>
2022-01-24 18:02:45 +0000

    removing the std alone func files, ported over to the preprocessing file

--- pangolin/utils/hash_functions.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import hashlib
-from Bio import SeqIO
-
-def get_hash_string(record):
-    seq = str(record.seq).upper().encode()
-    hash_object = hashlib.md5(seq)
-    hash_string = hash_object.hexdigest()
-    return hash_string
