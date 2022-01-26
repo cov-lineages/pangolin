3d446b82f3641a33aec1d2357b927d49fc2d9a85
aineniamh <aine.otoole@ed.ac.uk>
2022-01-26 10:54:46 +0000

    adding final report generation- logic to be further tested

--- pangolin/utils/report_collation.py
+++ pangolin/utils/report_collation.py
@@ -2,6 +2,8 @@
 
 import re
 import csv
+import json
+from pangolin.utils.config import *
 
 def usher_parsing(usher_result,output_report):
     """
@@ -10,7 +12,7 @@ def usher_parsing(usher_result,output_report):
     """
 
     with open(output_report, "w") as fw:
-        fw.write("taxon,lineage,conflict,usher_note\n")
+        fw.write("hash,lineage,conflict,usher_note\n")
 
         with open(usher_result, "r") as f:
             for l in f:
@@ -55,7 +57,7 @@ def pangolearn_parsing(pangolearn_result,output_report):
     name, lineage, conflict, ambiguity_score, pangolearn_note
     """
     with open(output_report,"w") as fw:
-        fw.write("taxon,lineage,conflict,ambiguity_score,pangolearn_note\n")
+        fw.write("hash,lineage,conflict,ambiguity_score,pangolearn_note\n")
 
         with open(pangolearn_result, "r") as f:
             reader = csv.DictReader(f)
@@ -104,118 +106,106 @@ def get_inference_dict(inference_csv):
     with open(inference_csv,"r") as f:
         reader = csv.DictReader(f)
         for row in reader:
-            inference_dict[row["taxon"]] = row
+            inference_dict[row["hash"]] = row
     return inference_dict
 
-def parse_scorpio_constellation(row, inference_out, voc_list):
 
-    if row["taxon"] in voc_dict:
-        scorpio_call_info = voc_dict[row["taxon"]]
-        new_row["scorpio_call"] = scorpio_call_info["constellations"]
-        new_row["scorpio_support"] = scorpio_call_info["support"]
-        new_row["scorpio_conflict"] = scorpio_call_info["conflict"]
-        new_row["note"] = f'scorpio call: Alt alleles {scorpio_call_info["alt_count"]}; Ref alleles {scorpio_call_info["ref_count"]}; Amb alleles {scorpio_call_info["ambig_count"]}; Oth alleles {scorpio_call_info["other_count"]}'
+def get_voc_list(voc_file):
+    voc_list = []
+    with open(voc_file,"r") as f:
+        for line in f:
+            voc_list.append(line.rstrip())
+    return voc_list
 
-        scorpio_lineage = scorpio_call_info["mrca_lineage"]
-        expanded_scorpio_lineage = expand_alias(scorpio_lineage, alias_dict)
-        expanded_pango_lineage = expand_alias(row['lineage'], alias_dict)
-        if '/' not in scorpio_lineage:
-            if expanded_scorpio_lineage and expanded_pango_lineage and not expanded_pango_lineage.startswith(expanded_scorpio_lineage):
-                new_row["note"] += f'; scorpio replaced lineage assignment {row["lineage"]}'
-                new_row['lineage'] = scorpio_lineage
-            elif "incompatible_lineages" in scorpio_call_info and row['lineage'] in scorpio_call_info["incompatible_lineages"].split("|"):
-                new_row["note"] += f'; scorpio replaced lineage assignment {row["lineage"]}'
-                new_row['lineage'] = scorpio_lineage
-    else:
-        expanded_pango_lineage = expand_alias(row['lineage'], alias_dict)
-        while expanded_pango_lineage and len(expanded_pango_lineage) > 3:
-            for voc in voc_list:
-                if expanded_pango_lineage.startswith(voc + ".") or expanded_pango_lineage == voc:
-                    # have no scorpio call but a pangolearn voc/vui call
-                    new_row['note'] += f'pangoLEARN lineage assignment {row["lineage"]} was not supported by scorpio'
-                    new_row['lineage'] = UNASSIGNED_LINEAGE_REPORTED
-                    new_row['conflict'] = ""
-                    new_row['ambiguity_score'] = ""
-                    break
-            if new_row['lineage'] == UNASSIGNED_LINEAGE_REPORTED:
-                break
-            expanded_pango_lineage = ".".join(expanded_pango_lineage.split(".")[:-1])
+def generate_final_report(preprocessing_csv, inference_csv, alias_file, voc_list, pango_version,analysis_mode, output_report):
+    """
+    preprocessing_csv header is: 
+    ["name","hash","lineage","scorpio_constellations",
+    "scorpio_mrca_lineage","scorpio_incompatible_lineages",
+    "scorpio_support","scorpio_conflict","scorpio_notes",
+    "designated","qc_status","qc_notes"]
 
-def generate_final_report(preprocessing_csv, inference_csv, alias_file, output_report):
+    inference_csv header is:
+    usher: taxon,lineage,conflict,usher_note
+    pangolearn: taxon,lineage,conflict,ambiguity_score,pangolearn_note
+
+    """
+    # the lineage aliases
     alias_dict = get_alias_dict(alias_file)
+
+    # the output of pangolearn/ usher inference pipelines
+    # only pass qc records present in this file
     inference_dict = get_inference_dict(inference_csv)
-    
-    with open(preprocessing_csv, "r") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
 
-            lineage_note = ""
-            version = ""
+    if analysis_mode == "pangolearn":
+        version = f"PLEARN-{pango_version}"
+    elif analysis_mode == "usher":
+        version = f"PUSHER-{pango_version}"
 
-            if row["hash"] in inference_dict:
-                inference_out = inference_dict[row["hash"]]
+    with open(output_report, "w") as fw:
+        # the output of preprocessing csv, all records present in this file
+        with open(preprocessing_csv, "r") as f:
+            reader = csv.DictReader(f)
 
-                #1. check if hash assigned
-                if row["designated"] == "True":
-                    lineage_note = "Assigned from designation hash."
-                    version = f"PANGO-{config['pango_version']}"
-                
-                #2. check if scorpio assigned
-                elif row["scorpio_call"]:
-                    scorpio_lineage = row["scorpio_mrca_lineage"]
-                    expanded_scorpio_lineage = expand_alias(scorpio_lineage, alias_dict)
+            out_header = reader.fieldnames
+            for field in ["conflict","ambiguity_score",f"{analysis_mode}_note","version","note"]:
+                out_header.append(field)
+            writer = csv.DictWriter(fw, fieldnames=out_header, lineterminator="\n")
+            writer.writeheader()
+
+            for row in reader:
+                new_row = row
+                new_row["version"] = version
+                new_row["note"] = ""
+
+                # if it passed qc and mapped
+                if row["hash"] in inference_dict:
+                    inference_out = inference_dict[row["hash"]]
                     expanded_pango_lineage = expand_alias(inference_out["lineage"], alias_dict)
 
+                    #1. check if hash assigned
+                    if row["designated"] == "True":
+                        new_row["note"] = "Assigned from designation hash."
+                        new_row["version"] = f"PANGO-{pango_version}"
+                    
+                    #2. check if scorpio assigned
+                    elif row["scorpio_mrca_lineage"]:
+                        scorpio_lineage = row["scorpio_mrca_lineage"]
+                        expanded_scorpio_lineage = expand_alias(scorpio_lineage, alias_dict)
 
+                        if '/' not in scorpio_lineage:
+                            if expanded_scorpio_lineage and expanded_pango_lineage and not expanded_pango_lineage.startswith(expanded_scorpio_lineage):
+                                new_row["note"] =  f'scorpio replaced lineage inference {inference_out["lineage"]}'
+                                new_row["lineage"] = scorpio_lineage
+                                
+                            elif row["scorpio_incompatible_lineages"] and inference_out["lineage"] in row["scorpio_incompatible_lineages"].split("|"):
+                                new_row["note"] = f'scorpio replaced lineage assignment {inference_out["lineage"]}'
+                                new_row["lineage"] = scorpio_lineage
 
+                    #3. check if lineage is a voc
+                    elif row["lineage"] in voc_list:
+                        while expanded_pango_lineage and len(expanded_pango_lineage) > 3:
+                            for voc in voc_list:
+                                if expanded_pango_lineage.startswith(voc + ".") or expanded_pango_lineage == voc:
+                                    # have no scorpio call but an inference voc/vui call
+                                    new_row['note'] = f'Lineage inference {inference_out["lineage"]} was not supported by scorpio'
+                                    new_row['lineage'] = UNASSIGNED_LINEAGE_REPORTED
+                                    new_row['conflict'] = ""
+                                    new_row['ambiguity_score'] = ""
+                                    break
 
+                            if new_row['lineage'] == UNASSIGNED_LINEAGE_REPORTED:
+                                break
 
-            else:
-                inference_out = None
-            
+                            expanded_pango_lineage = ".".join(expanded_pango_lineage.split(".")[:-1])
+                    
+                    #4. otherwise use inference output
+                    else:
+                        for field in inference_out:
+                            new_row[field] = inference_out[field]
+                        new_row['note'] = f"Assigned using {analysis_mode} inference."
 
+                else:
+                    new_row["lineage"] = UNASSIGNED_LINEAGE_REPORTED
 
-
-
-    ## Catching scorpio and usher output 
-    with open(output_report, "w") as fw:
-        fw.write("taxon,lineage,conflict,ambiguity_score,scorpio_call,scorpio_support,scorpio_conflict,version,pangolin_version,pangoLEARN_version,pango_version,status,note\n")
-        
-        version = f"PANGO-{config['pango_version']}"
-        with open(input.designated,"r") as f:
-            reader = csv.DictReader(f)
-            note = "Assigned from designation hash."
-            for row in reader:
-                
-                fw.write(f"{row['taxon']},{row['lineage']},,,,,,{version},{config['pangolin_version']},{config['pangoLEARN_version']},{config['pango_version']},passed_qc,{note}\n")
-                passed.append(row['taxon'])
-
-
-        with open(input.cached,"r") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                fw.write(f"{row['taxon']},{row['lineage']},{row['conflict']},{row['ambiguity_score']},{row['scorpio_call']},{row['scorpio_support']},{row['scorpio_conflict']},{row['version']},{row['pangolin_version']},{row['pangoLEARN_version']},{row['pango_version']},{row['status']},{row['note']}\n")
-                passed.append(row['taxon'])
-
-        version = f"PUSHER-{config['pango_version']}"
-
-        version = f"PANGO-{config['pango_version']}"
-        ## Catching sequences that failed qc in the report
-        for record in SeqIO.parse(input.qcfail,"fasta"):
-            desc_list = record.description.split(" ")
-            note = ""
-            for i in desc_list:
-                if i.startswith("fail="):
-                    note = i.lstrip("fail=")
-
-            fw.write(f"{record.id},None,,,,,,{version},{config['pangolin_version']},{config['pangoLEARN_version']},{config['pango_version']},fail,{note}\n")
-        
-        for record in SeqIO.parse(input.qc_pass_fasta,"fasta"):
-            if record.id not in passed:
-                fw.write(f"{record.id},None,,,,,,{version},{config['pangolin_version']},{config['pangoLEARN_version']},{config['pango_version']},fail,failed_to_map\n")
-
-    print(green(f"Output file written to: ") + f"{output.csv}")
-    if config[KEY_ALIGNMENT_OUT]:
-        print(green(f"Output alignment written to: ") + config[KEY_OUTDIR] +"/sequences.aln.fasta")
-
-
+                writer.writerow(new_row)
