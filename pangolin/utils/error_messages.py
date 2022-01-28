86f6bd4eea4f532363c6be66b9d8ad92749a0612
aineniamh <aine.otoole@ed.ac.uk>
2022-01-28 15:52:28 +0000

    ported function elsewhere

--- pangolin/utils/error_messages.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pangolin.utils.log_colours import green,cyan
-
-
-def install_error(package, url):
-    sys.stderr.write(cyan(f'Error: please install `{package}` with \n') +
-    f"pip install git+{url}")
-    sys.exit(-1)
